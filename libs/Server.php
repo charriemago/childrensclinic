cd1eb86e4fa26712b218ae66eb2b2596e087db4b
charriemago <46962614+charriemago@users.noreply.github.com>
2019-01-28 21:58:56 +0800

    Update Server.php

--- libs/Server.php
+++ libs/Server.php
@@ -7,7 +7,7 @@
 	ini_set('memory_limit', '-1');
 	ini_set('max_execution_time', 300);
 
-	define('DATABASE_HOST', 'dt0017');
+	define('DATABASE_HOST', 'localhost');
 	define('DATABASE_USER', 'root');
 	define('DATABASE_PASS', '');
 	define('DATABASE_NAME', 'db_clinic');
